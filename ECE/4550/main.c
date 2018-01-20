07748e08f1ca077cf6a4b079fdcbc7ba6dc92f54
Nelson Raphael <NelsonRaphael27@gmail.com>
2018-01-20 12:51:41 -0500

    Last min changes

--- ECE/4550/main.c
+++ ECE/4550/main.c
@@ -14,7 +14,7 @@ Learning GPIO implementation with a push button
 int main(void) {
 
 /*
-PB1 GPIO 17/88
+PB1 GPIO 17/52
 According to TRF it is manipulated by bits 2-3 in GPAMUX2
 
 PB2 GPIO 40/82
@@ -30,6 +30,7 @@ GPIO 09/49 LED 1
 According to TRF it is manipulated by bits 18-19 GPAMUX1
 
 GPIO 11/73 LED 2
+According to TRF it is manipulated by bits 22-23 GPAMUX1
 
 MUX
 
@@ -43,7 +44,7 @@ GpioCtrlRegs.GPAM
 
 while(1) {
 
-GpioDataRegs.GPASET.bit.GPIO17 /*  Sets it high - use this instead of writing one to it*/
+GpioDataRegs.GP52SET.bit.GPIO17 /*  Sets it high - use this instead of writing one to it for the pb1*/
 
 GpioDataRegs.GP?CLEAR.bit.GPIO? /* Sets it low - use this instead of writing zero to it*/
 
