f1b82a1e23b7a86c94d2432321f59a847c1830c5
Nelson Raphael <NelsonRaphael27@gmail.com>
2018-01-20 11:04:07 -0500

    Adding the GPIO assignments for the PBs and LEDs

--- ECE/4550/main.c
+++ ECE/4550/main.c
@@ -13,6 +13,28 @@ Learning GPIO implementation with a push button
 
 int main(void) {
 
+/* PB1 GPIO 17/88
+
+Digital for Arduion PB88
+
+MUX
+
+2 LED Activation
+
+GPIO 09/78 LED 1
+
+GPIO 11/79 LED 2
+
+MUX
+
+
+*/
+
+/*
+PB2 GPIO 48/30
+
+*/
+GpioCtrlRegs.GP17MUX.bit.GPIO
 return 0;
 
 }
